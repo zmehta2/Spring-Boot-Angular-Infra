a9401deff182023721671655b3bbd3b221113e6f
zmehta2 <zmehta2@dons.usfca.edu>
2025-03-03 06:17:48 -0800

    bug fixing

--- scripts/ecr-login.sh
+++ scripts/ecr-login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Logs into ECR
 
 AWS_REGION=${1:-us-east-1}
+ECR_REGISTRY=$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.$AWS_REGION.amazonaws.com
 
 # Get ECR authentication token and login
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
